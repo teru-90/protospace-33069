50332181f6b165a33029f746476c0fdc6eee180c
teru-90 <papaya600211@gmail.com>
2021-01-09 22:16:54 +0900

    ユーザー詳細画面は、そのユーザーが投稿したプロトタイプが表示されるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.includes(:user)
+    @prototypes =  @user.prototypes
   end
 
 end
